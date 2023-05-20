ad28c56a00d26e61149303eb4f1bf1c7b367e479
Keenan Nelson <keenanhnelson@gmail.com>
2023-05-20 16:59:47 -0700

    Updated remove glare position

--- Src/Webcam.cpp
+++ Src/Webcam.cpp
@@ -60,31 +60,11 @@ cv::Mat Webcam::TakePicWithLED(bool LED1State, bool LED2State){
 
 //Copys non-glare from one image to the glare part of another
 cv::Mat Webcam::RemoveLED_Glare(cv::Mat ImgInput1, cv::Mat ImgInput2){
-	//LED set that lights up in a square shape
-	//cv::Rect TopLeft(160,120,70,70);
-	//cv::Rect TopRight(340,130,70,70);
-	//cv::Rect BotRight(330,260,70,70);
-	//cv::Rect BotLeft(155,250,70,70);
-    //
-	//cv::Mat ImgTopLeft = ImgInput2(TopLeft);
-	//cv::Mat ImgTopRight = ImgInput2(TopRight);
-	//cv::Mat ImgBotRight = ImgInput2(BotRight);
-	//cv::Mat ImgBotLeft = ImgInput2(BotLeft);
-	//
-	//ImgTopLeft.copyTo(ImgInput1(TopLeft));
-	//ImgTopRight.copyTo(ImgInput1(TopRight));
-	//ImgBotRight.copyTo(ImgInput1(BotRight));
-	//ImgBotLeft.copyTo(ImgInput1(BotLeft));
-	//
-	//return(ImgInput1);
-	
-	
-	
 	//LED set that lights up in a diamond shape
-	cv::Rect Top(250,70,70,70);
-	cv::Rect Right(420,170,70,70);
-	cv::Rect Bot(260,280,70,70);
-	cv::Rect Left(80,180,70,70);
+	cv::Rect Top(230,60,70,70);
+	cv::Rect Right(400,160,70,70);
+	cv::Rect Bot(240,270,70,70);
+	cv::Rect Left(50,160,70,70);
 	
 	cv::Mat ImgTop = ImgInput1(Top);
 	cv::Mat ImgRight = ImgInput1(Right);
